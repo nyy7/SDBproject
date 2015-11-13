fdb154771d4ecd64d4e071bfd90ca7a304e63387
nyy <niyanyan7@gmail.com>
2015-11-13 14:55:23 -0500

    Android Project

---
+++ AccidentRecommender/app/src/main/java/com/example/nyy/accidentrecommender/ReportActivity.java
@@ -0,0 +1,80 @@
+package com.example.nyy.accidentrecommender;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nyy on 11/11/15.
+ */
+public class ReportActivity extends Activity {
+    public final static String EXTRA_uri = "com.example.nyy.test.uri";
+
+    EditText road_text;
+    EditText city_text;
+    EditText state_text;
+
+    MultiSelectionSpinner topic_spinner;
+    ProgressBar pb;
+
+    List<String> topics = new ArrayList<String>();
+    Info report = new Info();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        road_text = (EditText) findViewById(R.id.editText_road);
+        city_text = (EditText) findViewById(R.id.editText_city);
+        state_text = (EditText) findViewById(R.id.editText_state);
+
+        String[] topics = {"Rollover","Children/Minors Involved","Head-on","Alcohol or Drug Use","Teenaged Driver","Road Departure",
+                "Ejected from Vehicle","Seat Belt Usage","High-Speed/Excessive Speed","Hit and Run","Car Accident","Wrong-way Crash","Red Light/Stop Sign"};
+        topic_spinner = (MultiSelectionSpinner) findViewById(R.id.multispinner_topic_selection);
+        topic_spinner.setItems(topics);
+
+
+    }
+
+    public void submit(View view){
+        //set address
+        report.set_road(road_text.getText().toString());
+        report.set_city(city_text.getText().toString());
+        report.set_state(state_text.getText().toString());
+
+        //set topics
+        topics = topic_spinner.getSelectedItemsAsList();
+        String uri_submit = "http://52.24.92.50/cgi-bin/DataAnalytics/main.cgi?"
+                +"topics="+report.get_topics()
+                +"road="+report.get_road()
+                +"city="+report.get_city()
+                +"state="+report.get_state();
+        for (int i = 0; i < topics.size();i++) {
+            Log.d("MyTask",topics.get(i));
+        }
+        Intent report_intent = new Intent(this,ResultActivity.class);
+        report_intent.putExtra(EXTRA_uri,uri_submit);
+        startActivity(report_intent);
+
+
+    }
+
+}
---
+++ AccidentRecommender/app/src/main/java/com/example/nyy/accidentrecommender/Top3Info_XMLParser.java
@@ -0,0 +1,85 @@
+package com.example.nyy.accidentrecommender;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nyy on 11/12/15.
+ */
+public class Top3Info_XMLParser {
+    public static List<Top3Info> parseFeed(String content){
+        try {
+
+            boolean inDataItemTag = false;
+            String currentTagName = "";
+            Top3Info top3Info = null;
+            List<Top3Info> top3Infos = new ArrayList<Top3Info>();
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new StringReader(content));
+
+            int eventType = parser.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        currentTagName = parser.getName();
+                        if (currentTagName.equals("Accident")) {
+                            inDataItemTag = true;
+                            top3Info = new Top3Info();
+                            top3Infos.add(top3Info);
+                        }
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        if (parser.getName().equals("/Accident")) {
+                            inDataItemTag = false;
+                        }
+                        currentTagName = "";
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        if (inDataItemTag && top3Info != null) {
+                            switch(currentTagName) {
+                                case "ID":
+                                    top3Info.set_caseId(Integer.parseInt(parser.getText()));
+                                    break;
+                                case "ROAD":
+                                    top3Info.set_road(parser.getText());
+                                    break;
+                                case "CITY":
+                                    String city_state = parser.getText();
+                                    String[] cs = city_state.split(",");
+                                    top3Info.set_city(cs[0]);
+                                    top3Info.set_state(cs[1]);
+                                    break;
+                                case "LINK":
+                                    top3Info.set_link(parser.getText());
+                                    break;
+                                case "SIMILARITY":
+                                    top3Info.set_similarity(Double.parseDouble(parser.getText()));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        break;
+                }
+
+                eventType = parser.next();
+
+            }
+
+            return top3Infos;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}

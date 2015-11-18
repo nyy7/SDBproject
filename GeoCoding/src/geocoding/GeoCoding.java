26186d5d648a677fa7d8392cc78f6965301c1eea
洋伶 欧 <yanglingou@yous-MacBook-Pro.local>
2015-11-18 12:07:26 -0500

    new version

--- GeoCoding/src/geocoding/GeoCoding.java
+++ GeoCoding/src/geocoding/GeoCoding.java
@@ -23,6 +23,10 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -42,10 +46,6 @@ public class GeoCoding {
         String password = "Cs6604GIS";
 
         // query sentences
-        String query1 = "select road,city from accident where id=1; ";
-        ResultSet resultset = null;
-        String address = null;
-
         // Connect to the database on Amazon Cloud
         try {
             System.out.println("Loading driver...");
@@ -79,38 +79,78 @@ public class GeoCoding {
 
         // Excute query
         try {
+            String query1 = "select id, road,city from accident13 where geocenter is null;";
+            ResultSet resultset = null;
+            String address = null;
+
             Statement setupStatement = connection.createStatement();
             resultset = setupStatement.executeQuery(query1);
-            resultset.next();
-            address = resultset.getString(1) + ", " + resultset.getString(2);
-            System.out.println(address);
+
+            while (resultset.next()) {
+                address = resultset.getString(2) + ", " + resultset.getString(3);
+                
+                try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+                    String url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+                    url += URLEncoder.encode(address, "UTF-8");
+                    url += "&components=" + URLEncoder.encode("route", "UTF-8");
+                    url += "&key=" + URLEncoder.encode("AIzaSyBO8ALsTiK5_4rvejOHePz4OoqF_lCrfCw", "UTF-8");
+                    HttpGet get = new HttpGet(url);
+                    get.addHeader("Accept", "application/json");
+                    get.addHeader("Content-Type", "application/json");
+                    HttpResponse responsePost = client.execute(get);
+                    String jsonstring = EntityUtils.toString(responsePost.getEntity(), "UTF-8");
+                    System.out.println(address);
+                    
+
+                    try {
+                        JSONParser jparser = new JSONParser();
+                        JSONObject response_jo = (JSONObject) jparser.parse(jsonstring);
+                        if (new String(response_jo.get("status").toString()).equals("OK")){
+                        JSONArray results_ja = (JSONArray) response_jo.get("results");
+                        
+                        JSONObject jo2 = (JSONObject) results_ja.get(0);
+                        JSONObject geometry_jo = (JSONObject) jo2.get("geometry");
+                        JSONObject location_jo = (JSONObject) geometry_jo.get("location");
+                        String lat = location_jo.get("lat").toString();
+                        String lng = location_jo.get("lng").toString();
+                        
+                        String query2="update accident13 set geocenter=point("+lng+","+lat+") where id="+resultset.getString(1)+";";
+                        Statement setupStatement2 = connection.createStatement();
+                        setupStatement2.executeUpdate(query2);
+                        setupStatement2.close();
+                        } else {
+                            System.out.println("id="+resultset.getString(1));
+                            System.out.println(response_jo.get("status").toString());
+                        }
+                        
+                            
+                        
+                
+
+                    } catch (ParseException ep) {
+                        System.err.println(address);
+                        System.err.println(ep.toString());
+                    }
+
+                } catch (IOException ex) {
+                    System.err.println(address);
+                    System.out.println("Http Request Error!");
+                    System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
+                }
+
+            };
+
             setupStatement.close();
             connection.close();
 
         } catch (SQLException e) {
-            System.out.println("Query Failed! query:" + query1);
+            System.out.println("questwrong");
             e.printStackTrace();
             return;
 
         }
 
         // Google geocoding API
-        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-            String url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-            url += URLEncoder.encode(address, "UTF-8");
-            url += "&key=" + URLEncoder.encode("AIzaSyAwweuAYTzMUZ73NeWxNSi-mHXOVZ2ju6M", "UTF-8");
-            HttpGet get = new HttpGet(url);
-            get.addHeader("Accept", "application/json");
-            get.addHeader("Content-Type", "application/json");
-            HttpResponse responsePost = client.execute(get);
-            String jsonstring = EntityUtils.toString(responsePost.getEntity(), "UTF-8");
-            System.out.println(jsonstring);
-
-        } catch (IOException ex) {
-            System.out.println("Http Request Error!");
-            System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
-        }
-
     }
 
 }
